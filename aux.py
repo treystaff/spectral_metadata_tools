38caa37f3e51e92813c57d876a9579c49dd6f6d7
treystaff <treystaff@gmail.com>
2016-04-23 17:00:55 -0500

    Removed processing of vegfraction. It was decided that this data product is of extremely questionable quality, so do not include.

--- aux.py
+++ aux.py
@@ -44,7 +44,7 @@ def create_aux_file(data_dict, key_dict, other_keys, dataset_id, path):
 
 def copy_otherfiles(in_dir, out_dir, filenames, scan_info):
     """Does the work of matching otherfiles and copying them to the appropriate directory"""
-    img_filenames = []  # Maintain a record of image filenames for the vegfrac file
+    img_filenames = []  # Maintain a record of image filenames for the file
     pic_dir = os.path.join(out_dir, 'Pictures')
     for project, date, rep, scan_number, _ in zip(*scan_info):
         # Create a regex for matching files corresponding to this.
@@ -63,7 +63,7 @@ def copy_otherfiles(in_dir, out_dir, filenames, scan_info):
                     shutil.copy2(os.path.join(in_dir, filename),
                                    pic_dir)
 
-                    # Maintain a record of image filenames for the vegfrac file
+                    # Maintain a record of image filenames for the file
                     img_filenames.append(filename)
                 else:
                     # Copy to base dir
@@ -100,8 +100,6 @@ def process_otherfiles(in_dir, cal_meta, loc_meta):
     """
     Copy appropriate pictures and raw data (.Upwelling, etc.) over to new reorganized directory.
 
-    Also processes vegfraction (calls process_vegfraction)
-
     Need: project name, date, rep name, x, y, and scan number.
      Probs just need project name, date, scan num.  Maybe just scan num?
 
@@ -113,16 +111,6 @@ def process_otherfiles(in_dir, cal_meta, loc_meta):
     """
     # Get a list of all filenames in in_dir
     filenames = os.walk(in_dir).next()[2]
-
-    # Check if a vegfraction file exists. If so, read the data.
-    vegfrac_fn = [f for f in filenames if f.lower() == 'vegfraction.txt']
-    #vegfrac_fn = [f for f in filenames if 'veg' in f.lower() and 'fraction' in f.lower() and f.endswith('.txt')]
-    if len(vegfrac_fn) == 1:
-        vegfrac_data = read_vegfraction(os.path.join(in_dir, vegfrac_fn[0]))
-    elif len(vegfrac_fn) > 1:
-        raise RuntimeError('More than one VegFraction file found in {0}!'.format(in_dir))
-    else:
-        vegfrac_data = False
 
     # Check if a log file exists. if so, read the data.
     logfile = [f for f in filenames if '_log.txt' in f.lower() or '_log.xls' in f.lower()]
@@ -148,10 +136,6 @@ def process_otherfiles(in_dir, cal_meta, loc_meta):
     # Do the calibration stuff first
     cal_dir = cal_meta['out_dir']
     scans_info = parse_scans_info(cal_meta['scans_info'])
-    image_filenames = copy_otherfiles(in_dir, cal_dir, filenames, scans_info)
-    # Process vegfrac for cal data
-    if vegfrac_data:
-        process_vegfraction(vegfrac_data, image_filenames, cal_dir)
     if logdata:
         process_logfile(logdata, scans_info, cal_dir)
 
@@ -160,85 +144,8 @@ def process_otherfiles(in_dir, cal_meta, loc_meta):
         meta_dict = loc_meta[loc]
         loc_dir = meta_dict['out_dir']
         scans_info = parse_scans_info(meta_dict['scans_info'])
-        image_filenames = copy_otherfiles(in_dir, loc_dir, filenames, scans_info)
-        if vegfrac_data:
-            process_vegfraction(vegfrac_data, image_filenames, loc_dir)
         if logdata:
             process_logfile(logdata, scans_info, loc_dir)
-
-
-def read_vegfraction(path):
-    """
-    Reads the vegfraction file. Currently only prints a warning for unexpected vegfraction header/footer.
-
-    Throws RuntimeError if data format is unexpected.
-
-    Relies on readData from utility.py
-
-    Parameters:
-        path - string. Full path to vegfraction file.
-
-    Returns:
-        header - List of strings.
-        data - List of strings. Vegfraction data.
-        footer - List of one string. Footer (should be processing date and time). None if no footer detected.
-    """
-    vf_data = readData(path)
-    header = vf_data[0]
-    if header[0].lower() != 'name':
-        print('Unexpected Vegfraction header from {0}!:\n{1}\n'.format(path, header))
-
-    # Define some picture suffixes
-    pic_sufs = ('.jpg', '.png', '.tif', '.bmp')
-    footer = vf_data[-1]
-    if not footer[0].lower().startswith('processing') and not footer[0].lower().endswith(pic_sufs):
-        print('Unexpected Vegfraction footer from {0}!:\n{1}\n'.format(path, footer))
-
-    if footer[0].lower().endswith(pic_sufs):
-        footer = None
-        data = vf_data[1:]
-    else:
-        data = vf_data[1:-1]
-
-    # Make sure the data is formatted as expected. Raise error otherwise
-    if not data[0][0].lower().endswith(pic_sufs):
-        import pdb
-        pdb.set_trace()
-        raise RuntimeError('Vegfraction data {0} not formatted as expected! '
-                           'First element is not image filename!'.format(path))
-
-    if len(data[0]) != len(header):
-        raise RuntimeError('Vegfration data {0} is not formatted as expected! '
-                           'Number of header elements does not match number of data elements!'.format(path))
-
-    return header, data, footer
-
-
-def process_vegfraction(vegfrac_data, img_filenames, out_dir):
-    """
-    Process vegfraction file
-
-    Parameters:
-        vegfrac_data - output from read_vegfration as a tuple (header, data, footer)
-        img_filenames - List of image filenames (return from copy_otherfiles()0
-        out_dir - Directory to place new vegfrac file.
-
-    Returns:
-        Nothing!
-    """
-    # Split up the vegfrac_data
-    header, data, footer = vegfrac_data
-
-    # Open a new vegfrac file in the output directory
-    with open(os.path.join(out_dir, 'VegFraction.csv'), 'w') as vegfrac_file:
-        writer = csv.writer(vegfrac_file)
-        writer.writerow(header)
-        for row in data:
-            if row[0] in img_filenames:
-                writer.writerow(row)
-
-        if footer is not None:
-            writer.writerow(footer)
 
 
 def read_log(path):
@@ -396,7 +303,6 @@ def process_xls_logfile(logdata, cal_meta, loc_meta):
             # Write the header
             for row in header:
                 writer.writerow(row)
-
 
             # Write the location info
             writer.writerow(loc_info)
